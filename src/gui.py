fa68c76d885afc01f9bffd01c43de365dcf146e5
Cooper Luce <16cluce@gmail.com>
2023-03-03 22:17:28 -0500

    Updated the gui file. I'm switching to pyglet.

--- src/gui.py
+++ src/gui.py
@@ -26,30 +26,50 @@ for imcolor in imcolors:
                                     default_value=imdata[3],
                                     tag=f"{imtag}")
 
+
+def change_text(sender, app_data):
+    """."""
+    dpg.set_value("mouseloc", f"Mouse Location: {app_data}")
+
+
+# with dpg.handler_registry():
+    # dpg.add_mouse_move_handler(callback=change_text)
+#     dpg.add_mouse_drag_handler(callback=change_text)
+
+with dpg.item_handler_registry(tag="widget handler") as handler:
+    dpg.add_item_clicked_handler(callback=change_text)
+
 size = 510
 with dpg.window(label="PyChess"):
+    dpg.add_text("Mouse Location", tag="mouseloc")
     with dpg.drawlist(width=size, height=size):
         # This is kind of a fun way to define the colors for now.
-        light = (243, 205, 170)
-        dark = tuple([255 - light[i] for i in range(3)])
-        line = tuple([(light[i] - dark[i]) / 2 for i in range(3)])
+        with dpg.draw_layer(tag="board_layer"):
 
-        color, oppcolor = light, dark
-        for xmin in range(0, 400, 50):
-            for ymin in range(0, 400, 50):
-                dpg.draw_rectangle((xmin, ymin),
-                                   (xmin + 50, ymin + 50),
-                                   fill=color,
-                                   color=line,
-                                   thickness=10)
+            light = (243, 205, 170)
+            dark = tuple([255 - light[i] for i in range(3)])
+            line = tuple([(light[i] - dark[i]) / 2 for i in range(3)])
+
+            color, oppcolor = light, dark
+            for xmin in range(0, 400, 50):
+                for ymin in range(0, 400, 50):
+                    dpg.draw_rectangle((xmin, ymin),
+                                       (xmin + 50, ymin + 50),
+                                       fill=color,
+                                       color=line,
+                                       thickness=10)
+                    color, oppcolor = oppcolor, color
                 color, oppcolor = oppcolor, color
-            color, oppcolor = oppcolor, color
-
-            # dpg.draw_image("black_bishop", (0, 0), (45, 45))
         s = 0
         for p in impieces:
-            dpg.draw_image(f"black_{p}", (s, 0), (s + 45, 45))
+            t = f"black_{p}"
+            dpg.draw_image(t, (s, 0), (s + 45, 45), tag=f"_{t}")
             s += 50
+
+
+# TODO: Map each square to a coordinate root
+
+dpg.bind_item_handler_registry("mouseloc", "widget handler")
 
 dpg.create_viewport(title='PyChess', width=size, height=size)
 dpg.setup_dearpygui()
